ea832212d9b174b12182de00172220dd305b5f51
Shtekata <gesheval@gmail.com>
2020-12-03 21:02:32 +0200

    remove test

--- test - Copy - Copy/src/app/shared/interfaces/new-theme.ts
+++
@@ -1,4 +0,0 @@
-export interface INewTheme {
-    themeName: string;
-    postText: string;
-}
--- test - Copy/src/app/shared/interfaces/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './base';
-export * from './theme';
-export * from './user';
-export * from './post';
-
